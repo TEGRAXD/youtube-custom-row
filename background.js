7cfab36059bb8fd41eee6e1f80ad9ef2623da947
TEGRAXD <58646818+TEGRAXD@users.noreply.github.com>
2025-06-15 14:32:44 +0700

    Fixed: automatically run when Youtube Homepage is refreshed

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
 // Required to register a service worker in manifest v3
+
+chrome.runtime.onInstalled.addListener(() => { // add listener is error
+    // Set default values for itemsPerRow and postsPerRow
+    chrome.storage.sync.set({ itemsPerRow: 5, postsPerRow: 5 }, () => {
+        // console.log("Default values set for itemsPerRow and postsPerRow.");
+    });
+});
--- content.js
+++ content.js
@@ -1,33 +1,17 @@
-chrome.storage.sync.get("itemsPerRow", ({ itemsPerRow }) => {
-    if (itemsPerRow) {
-        const styleId = "custom-row-style";
-        let style = document.getElementById(styleId);
-        if (!style) {
-                style = document.createElement("style");
-                style.id = styleId;
-                document.head.appendChild(style);
-        }
-        style.textContent = `
-            ytd-rich-grid-renderer {
-                --ytd-rich-grid-items-per-row: ${itemsPerRow} !important;
-            }
-        `;
+// content.js
+chrome.storage.sync.get(["itemsPerRow", "postsPerRow"], ({ itemsPerRow = 5, postsPerRow = 5 }) => {
+    const styleId = "custom-row-style";
+    let style = document.getElementById(styleId);
+    if (!style) {
+        style = document.createElement("style");
+        style.id = styleId;
+        document.head.appendChild(style);
     }
-});
 
-chrome.storage.sync.get("postsPerRow", ({ postsPerRow }) => {
-    if (postsPerRow) {
-        const styleId = "custom-row-style";
-        let style = document.getElementById(styleId);
-        if (!style) {
-                style = document.createElement("style");
-                style.id = styleId;
-                document.head.appendChild(style);
+    style.textContent = `
+        ytd-rich-grid-renderer {
+            --ytd-rich-grid-items-per-row: ${itemsPerRow} !important;
+            --ytd-rich-grid-posts-per-row: ${postsPerRow} !important;
         }
-        style.textContent = `
-            ytd-rich-grid-renderer {
-                --ytd-rich-grid-posts-per-row: ${postsPerRow} !important;
-            }
-        `;
-    }
+    `;
 });
